9763807e2b65ebddbf72ac2ef73a00f348915992
Griffin Beels <fartzilla2010@gmail.com>
2018-01-06 02:58:26 -0500

    griff revert commit

--- Bitcoin Brawl/GameScene.swift
+++ Bitcoin Brawl/GameScene.swift
@@ -18,9 +18,7 @@ class GameScene: SKScene {
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        label?.text = "Bitcoin Brawls"
         label?.text = "Bitcoin Brawl"
-        label?.text = "fuck grif"
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
